9262ca0344edc7a769d0571403625ffd4e834c14
Ekow Turkson <kowtee0@gmail.com>
2025-10-05 19:13:56 +0000

    Fix test function names for checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,14 +8,14 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up a calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test addition operation."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-2, -3), -5)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test subtraction operation."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
@@ -34,7 +34,6 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
-        # The divide method should return None for division by zero
         self.assertIsNone(self.calc.divide(5, 0))
 
 if __name__ == '__main__':
